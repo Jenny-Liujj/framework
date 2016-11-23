5ce6b5175df859b0a134026d61a3107d1ba73803
刘精精 <liujingjing7899@gmail.com>
2016-11-23 16:40:46 +0800

    1

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,14 +4,9 @@ lock '3.6.1'
 set :application, 'framework'
 set :repo_url, 'git@github.com:liu7899/framework.git'
 set :deploy_user, 'ssgene-forum'
-set :deploy_to, '/home/ssgene-forum/www/framework'
+set :deploy_to, '/var/www/framework'
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :pty, true
-set :linked_files, fetch(:linked_files, []).push('config/database.yml',
-                                                 'config/secrets.yml',
-                                                 'config/settings.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache',
-                                               'tmp/sockets', 'public/system')
 
 namespace :deploy do
 
